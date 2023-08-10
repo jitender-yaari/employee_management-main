be288f6dc24526de31a71acc67c52cc169da2c31
Jitender Yadav <jitender1137@gmail.com>
2023-08-10 08:04:35 +0530

    second commit

--- src/main/java/com/universal/em/crons/CommonCrons.java
+++ src/main/java/com/universal/em/crons/CommonCrons.java
@@ -23,7 +23,7 @@ public class CommonCrons {
 
 	@Scheduled(fixedRate = 1000 * 60 * 10)
 	public void scheduler() {
-		logger.info("Crons Hit Url {}", url);
+//		logger.info("Crons Hit Url {}", url);
 		try {
 			OkHttpClient client = new OkHttpClient().newBuilder().build();
 			Request request1 = new Request.Builder().url(url)
@@ -32,17 +32,8 @@ public class CommonCrons {
 			logger.info("Swagger Api hit sucessfully");
 			response.close();
 		}catch (Exception e){
-			try {
-				e.printStackTrace();
-				OkHttpClient client = new OkHttpClient().newBuilder().build();
-				Request request1 = new Request.Builder().url("https://www.google.com/")
-						.method("GET", null).addHeader("accept", "*/*").build();
-				Response response = client.newCall(request1).execute();
-				logger.info("Google Api hit sucessfully");
-				response.close();
-			}catch (Exception e1){
-				logger.info(e1.getMessage());
-			}
+			logger.info(e.getMessage());
+
 		}
 	}
 }
